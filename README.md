703022fe4b30e50d1d6e66447e896f6d92e0efde
Furkan Aktepe <7.furkanaktepe@gmail.com>
2024-07-19 16:51:27 -0400

    .

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # AutoRandom
 
-AutoRandom is an automated verifiable random number generator, 
-That shares a random number on a daily basis, utilizing Chainlink VRF and OpenZeppelin Defender's Automated Actions
+AutoRandom is an automated verifiable random number generator 
+that shares a random number on a daily basis, utilizing Chainlink VRF and OpenZeppelin Defender's Automated Actions
+
+Website: https://auto-random.vercel.app/
--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import contract from "./contract.js";
 import { useState, useEffect } from "react";
+import { Card, Container, Col, Row, Button } from "react-bootstrap";
+import { Text } from 'react-font'
+
 
 function App() {
 
   const [lastRandomNumber, setLastRandomNumber] = useState(0);
 
   const contractLink = `https://sepolia.etherscan.io/address/${process.env.REACT_APP_CONTRACT_ADDRESS}`;
+
+  const fontName = "Montserrat";
 
   const update = async () => {  
 
@@ -31,23 +36,74 @@ function App() {
   },[]);
 
   return (
-    <div className="App">
+    <div>
 
-      <h1>AutoRandom</h1>
+    <br/><br/>
+
+    <Container>
+
+      <Row>
+        <Col></Col>
+        <Col xs={5}><Text family={fontName}><h1 style={{color:"white", fontSize:"20mm"}}>
+         AutoRandom
+        </h1></Text></Col>
+        <Col></Col>
+      </Row>
+
+      <br/>
       
-      <h3><i>
-        AutoRandom is an automated verifiable random number generator, <br/>
-        That shares a random number on a daily basis, utilizing Chainlink VRF and OpenZeppelin Defender's Automated Actions
-      </i></h3>
+      <Row>
+        <Col></Col>
+        <Col xs={12}>
+          <h5 style={{color:"white", fontSize:"6mm"}}><i><Text family={fontName}>
+            AutoRandom is an automated verifiable random number generator
+            that shares a random number on a daily basis, utilizing Chainlink VRF and OpenZeppelin Defender's Automated Actions
+          </Text></i></h5>
+        </Col>
+        <Col></Col>
+      </Row>
+
+      <br/><br/><br/>
+
+      <Row>
+        <Col></Col>
+        <Col xs={12}>
+          
+        <Card 
+          style={{ width: '80rem' }} 
+          bg="light"
+          className="mb-2"
+          text="dark">
+          <Card.Body>
+            <Card.Title as="h1"><Text family={fontName}>Today's Random Number</Text></Card.Title>
+            <Card.Text as="h3">
+              <br/>
+              <Text family={fontName}>{lastRandomNumber}</Text>
+              <br/>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+          
+        </Col>
+        <Col></Col>
+      </Row>
+
+    <br/><br/><br/>
       
-      <br/><br/><br/><br/><br/><br/>
+    <Row>
+      <Col></Col>
+      <Col xs={8}>
+        <h4 style={{color:"white"}}><Text family={fontName}>
+          Contract Address: <a href={contractLink} 
+          rel="noreferrer" target="_blank">{process.env.REACT_APP_CONTRACT_ADDRESS}
+        </a></Text></h4>
+      </Col>
+      <Col></Col>
+      <Col xs={3}><Button href="https://github.com/furkanaktepe01/AutoRandom" target="_blank">See Github Repository</Button></Col>
+    </Row>
+
+    </Container>
       
-      <h4>Todays Random Number: {lastRandomNumber}</h4>
-      
-      <br/><br/><br/><br/><br/><br/>
-      
-      <h4>Contract Address: <a href={contractLink} rel="noreferrer" target="_blank">{process.env.REACT_APP_CONTRACT_ADDRESS}</a></h4>
-    
     </div>
   );
 }
